cdd2a7d4b20d32d95149bea960ace8befbeb346b
chen8114@sina.com <chen8114@sina.com>
2015-04-03 20:30:14 +0800

    update help

--- util/cmds.js
+++ util/cmds.js
@@ -2,6 +2,10 @@
  * Created by vip on 15/4/1.
  */
 module.exports = {
-    "init"     : "init" ,
-    "install"  : "install"
+    "init"      : "init" ,
+    "install"   : "install" ,
+    "help"      : "help" ,
+    "-help"     : "help" ,
+    "--help"    : "help" ,
+    "-h"        : "help"
 };
