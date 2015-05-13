a9ff84b4d4a83a13fad8b79f12f509cc4566c785
chen8114@sina.com <chen8114@sina.com>
2015-05-13 17:15:14 +0800

    delete bower-cli dependency

--- util/file.js
+++ util/file.js
@@ -1,18 +1,16 @@
-var fs      = require( "fs" ) ,
-    log     = require( "./log" ) ,
-    path    = require( "path" ) ,
+var log     = require( "./log" ) ,
     grunt   = require( "grunt" );
 
 var file    = module.exports    = {} ,
     pathSplitString             = /[\/|\\]/g;
 
 function copySingleFile( filePath , rootDir , targetDir ){
-    var _dir = filePath.split( pathSplitString ).slice( 0 , -1 ).join( "/" );
-    if( !grunt.file.exists( _dir ) ){
-        grunt.file.mkdir( _dir );
+    var _dir;
+    if( !grunt.file.exists( targetDir ) ){
+        grunt.file.mkdir( targetDir );
     }
     _dir            = filePath.replace( rootDir , targetDir );
-    grunt.file.copy( filePath , _dir );
+    grunt.file.copy( filePath , _dir , { force : true } );
 }
 
 
@@ -20,9 +18,9 @@ function copySingleFile( filePath , rootDir , targetDir ){
  *  允许递归文件夹式的copy
  */
 file.copy   = function( src , target ){
-    var _paths  = [];
-    src     = src.replace( /[\\|\/]$/ , "" );
-    target  = target.replace( /[\\|\/]$/ , "" );
+    var _paths      = [],
+        _regIsDir   = /[\\|\/]$/;
+    src     = src.replace( /^(\.[\\|\/])/ , "" );
     if( grunt.file.isFile( src ) ){
         grunt.file.copy( src , target );
     } else if( grunt.file.isDir( src ) ){
@@ -33,6 +31,11 @@ file.copy   = function( src , target ){
                 log( filepath );
             }
         } );
+        if( !_regIsDir.test( src ) ){
+            if( _regIsDir.test( target ) ){
+                target     = target + src.replace( /^.*[\\|\/]/ , "" );
+            }
+        }
         for( var i = 0 , len = _paths.length; i < len; i++ ){
             copySingleFile( _paths[ i ] , src , target );
         }
